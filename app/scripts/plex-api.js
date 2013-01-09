f97e9425a2fa9eedad280d66795cc949b4790aba
Jakob Hilarius <jakob@syscall.dk>
2013-01-10 00:00:39 +0100

    Prepared for myPlex integration

--- app/scripts/plex-api.js
+++ app/scripts/plex-api.js
@@ -1,5 +1,12 @@
 function PlexAPI() {
     'use strict';
+
+    function getBasicAuthToken(user, password) {
+        var token = user + ':' + password;
+        var encoded = Base64.encode(token);
+        return 'Basic ' + encoded;
+    }
+
 	this.browse = function(url, callback) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
@@ -89,6 +96,61 @@ function PlexAPI() {
         return 'http://'+Settings.getPMS()+':32400/photo/:/transcode?width='+width+'&height='+height+'&url='+encodeURIComponent(url);
     };
 
+    /**
+     * Login to myPlex and return the authentation token.
+     *
+     * @param {string} username myPlex username
+     * @param {string} password myPlex password
+     * @param {function} callback the callback function that is called with the auth token.
+     */
+    this.login = function (username, password, callback) {
+        var auth = getBasicAuthToken(username, password);
+        var url = 'https://my.plexapp.com/users/sign_in.xml';
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    var tokenNode = xhr.responseXML.getElementsByTagName('authentication-token')[0];
+                    callback(tokenNode.childNodes[0].nodeValue);
+                }
+            }
+        };
+        xhr.open('POST',url);
+        xhr.setRequestHeader('Authorization', auth);
+        xhr.setRequestHeader('X-Plex-Client-Identifier', 'nettv-plex-client'); // TODO: should be device unique
+        xhr.setRequestHeader('X-Plex-Platform', 'NetTV');
+        //xhr.setRequestHeader('X-Plex-Platform-Version', '');
+        xhr.setRequestHeader('X-Plex-Provides', 'player');
+        xhr.setRequestHeader('X-Plex-Product', 'nettv-plex-client');
+        //xhr.setRequestHeader('X-Plex-Version', '');
+        //xhr.setRequestHeader('X-Plex-Device', ''); // TODO: example Beoplay V1
+        xhr.send(null);
+    };
+
+    /**
+     * List the available servers.
+     *
+     * @param {string} token the authentication token
+     * @param {function} callback the callback function that is called with the list of servers
+     */
+    this.servers = function (token, callback) {
+
+        var url = 'https://my.plexapp.com/pms/servers.xml?X-Plex-Token='+token;
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    console.log(xhr.responseXML);
+                }
+            }
+        };
+        xhr.open('POST',url);
+
+        xhr.setRequestHeader('X-Plex-Client-Identifier', 'nettv-plex-client'); // TODO: should be device unique
+        xhr.send(null);
+    };
 }
 
 // Register the API globally
